faccbf8eaef97e3db1f2bc52e6ebeb8c4901cda7
Ibrahim khan <ibrahim2013khan@gmail.com>
2024-05-18 16:13:03 +0600

    React Practice

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -1,5 +1,4 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'  
 import { FaFacebook, FaYoutube } from "react-icons/fa";
 import './App.css'
@@ -65,9 +64,6 @@ function App() {
       <div>
         <a href="https://vitejs.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
       {/*Logo end*/}
