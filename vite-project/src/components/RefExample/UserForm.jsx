e79bba8d0c965ec0acd297017c312aed7c57d233
Ibrahim khan <ibrahim2013khan@gmail.com>
2024-06-02 01:49:27 +0600

    React Practice

--- vite-project/src/components/RefExample/UserForm.jsx
+++ vite-project/src/components/RefExample/UserForm.jsx
@@ -1,18 +1,33 @@
-import React, { Component } from 'react'
+import React, { Component, createRef } from 'react'
 
 export default class UserForm extends Component {
+
+    constructor(props){
+        super(props);
+        this.userNameRef = createRef();
+        this.state = {};
+    }
+
+    handleSubmit = (event) => {
+        event.preventDefault();
+        console.log(this.userNameRef.current.value);
+        this.userNameRef.current.style.
+        backgroundColor = "green";
+    };
+
   render() {
     return (
       
-        <form>
+        <form onSubmit={this.handleSubmit}>
             <div className="form-field">
                 <label htmlFor="userName">UserName : </label>
-                <input type="text" id="userName"/>
+                <input type="text" id="userName" ref={this.userNameRef}/>
             </div>
             <div className="form-field">
                 <label htmlFor="Password">Password : </label>
                 <input type="Password" id="Password"/>
             </div>
+            <button type="submit">Register</button>
         </form>
       
     )
